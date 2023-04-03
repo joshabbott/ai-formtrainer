97272de2c7b378ae568479d80fad7985c2e0d302
josh abbott <45774251+joshabbott@users.noreply.github.com>
2023-04-02 20:52:26 -0400

    Initial push, some demos and examples
    Demos using webcam to count number of curls based on elbow angle and states, and looking at static images of squats to determine proper angles of different states (start, finish, in betweens)

---
+++ demo_code/squats-hip-angle.py
@@ -0,0 +1,60 @@
+# squats-hip-angle.py
+
+# COMPUTE AND DISPLAY RIGHT HIP ANGLE FROM RIGHT SHOULDER TO RIGHT KNEE
+
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle 
+
+training_dir = './training_set'
+files = ['1-sit-to-stand-a_h.jpg','2-sit-to-stand-b_h.jpg','3-sit-to-stand-c_h.jpg','7-kb-goblet-squat-a_h.jpg','8-kb-goblet-squat-b_h.jpg']
+filelist = [training_dir+'/'+filename for filename in files]
+
+for file in filelist: 
+	with mp_pose.Pose(static_image_mode=True, model_complexity=2,enable_segmentation=True,min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+		# Read the file in and get dims
+		image = cv2.imread(file)
+
+		# Convert the BGR image to RGB and then process with the `Pose` object.
+		results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+		# Extract landmarks
+		try:
+			landmarks = results.pose_landmarks.landmark
+
+			# Get coordinates
+			shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+			hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+			knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+
+			# Calculate angle
+			angle = calculate_angle(shoulder, hip, knee)
+			print("file: "+str(file)+", hip angle: "+str(angle))
+
+			# Visualize angle
+			cv2.putText(image, str(angle),tuple(np.multiply(hip, [2400, 1350]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+		except:
+			pass
+
+
+		# Render detections
+		mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,mp_drawing.DrawingSpec(color=(245,117,66), thickness=4, circle_radius=4),mp_drawing.DrawingSpec(color=(245,66,230), thickness=4, circle_radius=4))
+
+		# # Save image with drawing
+		filename = "./output/"+file.split('/')[-1]
+		print(filename)
+		cv2.imwrite(filename, image)
