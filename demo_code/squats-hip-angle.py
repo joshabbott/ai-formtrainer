9f6232da5f3678701c4997d0c604f60729cad4ca
josh abbott <45774251+joshabbott@users.noreply.github.com>
2023-04-11 23:05:44 -0400

    updated example squats image processing code
    updated to annotate any image size properly and segment out person

--- demo_code/squats-hip-angle.py
+++ demo_code/squats-hip-angle.py
@@ -29,6 +29,7 @@ for file in filelist:
 	with mp_pose.Pose(static_image_mode=True, model_complexity=2,enable_segmentation=True,min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
 		# Read the file in and get dims
 		image = cv2.imread(file)
+		dims = image.shape
 
 		# Convert the BGR image to RGB and then process with the `Pose` object.
 		results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
@@ -42,17 +43,25 @@ for file in filelist:
 			knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
 
 			# Calculate angle
-			angle = calculate_angle(shoulder, hip, knee)
+			angle = float("{:.2f}".format(calculate_angle(shoulder, hip, knee)))
 			print("file: "+str(file)+", hip angle: "+str(angle))
 
 			# Visualize angle
-			cv2.putText(image, str(angle),tuple(np.multiply(hip, [2400, 1350]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+			cv2.putText(image, str(angle),tuple(np.multiply(hip, [dims[1]+20, dims[0]-50]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 		except:
 			pass
 
 
+
+		# segment person out of image
+		red_img = np.zeros_like(image,dtype=np.uint8)
+		red_img[:,:] = (255,255,255)
+		segm_2class = 0.2 + 0.8*results.segmentation_mask
+		segm_2class = np.repeat(segm_2class[...,np.newaxis],3,axis=2)
+		image = image * segm_2class + red_img * (1 - segm_2class)
+
 		# Render detections
-		mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,mp_drawing.DrawingSpec(color=(245,117,66), thickness=4, circle_radius=4),mp_drawing.DrawingSpec(color=(245,66,230), thickness=4, circle_radius=4))
+		mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,mp_drawing.DrawingSpec(color=(245,117,66), thickness=10, circle_radius=4),mp_drawing.DrawingSpec(color=(245,66,230), thickness=10, circle_radius=4))
 
 		# # Save image with drawing
 		filename = "./output/"+file.split('/')[-1]
